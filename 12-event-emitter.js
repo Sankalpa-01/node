774864165e72f575bb762666654b89b6bd92bd37
Sankalpa_01 <sankalpaanda@gmail.com>
2025-03-26 18:00:50 +0530

    event emitter done correctly

---
+++ 12-event-emitter.js
@@ -0,0 +1,17 @@
+const EventEmitter = require('events');
+
+const customEmitter = new EventEmitter()
+
+// first on then emit must be there
+customEmitter.on('response', (name, id) => {
+    console.log(`data received user ${name} with id: ${id}`);
+})
+
+customEmitter.on('response', () => {
+    console.log(`some other logic here`);
+})
+
+customEmitter.emit('response', 'john', 34)
+
+// on => listen for an event
+// emit => emit an event
